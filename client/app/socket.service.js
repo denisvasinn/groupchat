9f55529fb862d5739471a467253c9601b03fd463
denisvasinn <denis.vasin@outlook.com>
2017-01-06 17:28:59 +0300

    tmp

--- client/app/socket.service.js
+++ client/app/socket.service.js
@@ -10,7 +10,7 @@ var Observable_1 = require("rxjs/Observable");
 var io = require("socket.io-client");
 var SocketService = (function () {
     function SocketService() {
-        this.uri = 'http://localhost:3000';
+        this.uri = 'https://simple-group-chat.herokuapp.com/';
     }
     SocketService.prototype.connect = function () {
         this.socket = io(this.uri);
